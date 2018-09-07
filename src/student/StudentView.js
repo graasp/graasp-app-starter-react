6e27db8a78ac258a72a3bd0a7b24995da8618168
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2018-09-07 10:59:48 +0200

    feat: show example of teacher and student views
    Users will be able to see how you can have two different views in the
    app according to the different query parameters passed through.
    
    close #3

---
+++ src/student/StudentView.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Alert, Container } from 'reactstrap';
+import Logo from '../logo.svg';
+
+const StudentView = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={Logo} className="App-logo" alt="Logo" />
+      <h1 className="App-title">Welcome to the Graasp App Starter Kit</h1>
+    </header>
+    <Container className="App-body">
+      <Alert color="info">
+        This is the student view. Switch to the teacher view by clicking on the URL below.
+        <a href='?mode=teacher'>
+          <pre>{`${window.location.host}/?mode=teacher`}</pre>
+        </a>
+      </Alert>
+    </Container>
+  </div>
+);
+
+export default StudentView;
