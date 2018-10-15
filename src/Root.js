79b2f70e08596b70769e2a60a11eab06e5da5e98
hassan <hassan.abdulghaffar@epfl.ch>
2018-10-15 18:23:44 +0200

    feat: add internationalizations
    add internationalization too the starter app and lang query strings
    
    close #19

---
+++ src/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import Header from './layout/Header';
+import i18nConfig from './config/i18n';
+import App from './App';
+
+const Root = () => (
+  <I18nextProvider i18n={i18nConfig}>
+    <Header />
+    <App />
+  </I18nextProvider>
+);
+export default Root;
